c81156f94b7cd371f3687f709c3534e5e6511d67
devflores-ka <luciano.floress@outlook.es>
2024-07-06 23:51:58 -0400

    Inicial commit

---
+++ UserData.sh
@@ -0,0 +1,24 @@
+# !/bin/bash
+set -e
+
+# Repositorio publico de GitHub
+GIT_REPO_URL="https://github.com/devflores-ka/django.git"
+
+# Nombre del directorio de la aplicacion
+PROJECT_MAIN_DIR_NAME="djproject"
+
+# Clonar repositorio
+git clone "$GIT_REPO_URL" "/home/ubuntu/$PROJECT_MAIN_DIR_NAME"
+
+# Cambiar de directorio
+cd "/home/ubuntu/$PROJECT_MAIN_DIR_NAME"
+
+# Dar permiso a los archivos .sh
+chmod +x scripts/*.sh
+
+# Instalar dependencias
+./scripts/instance_os_dependencies.sh
+./scripts/python_dependencies.sh
+./scripts/gunicorn.sh
+./scripts/nginx.sh
+./scripts/start_app.sh
