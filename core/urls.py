1e8bd1d45fd2061fa770e6a34646da88e9de93d2
devflores-ka <luciano.floress@outlook.es>
2024-07-07 00:59:34 -0400

    Actualización completa del proyecto con cambios en múltiples carpetas

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_app project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -23,6 +23,7 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.HomePageView.as_view(), name='home'),
 ]
 
 if settings.DEBUG:
